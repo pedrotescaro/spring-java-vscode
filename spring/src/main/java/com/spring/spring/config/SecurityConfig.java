f0c88536068fcd6aebbe8542ba52f1628fb1944d
Pedro Tescaro <pedroatescaro@gmail.com>
2025-12-15 22:15:52 -0300

    [feat] correção no layout e aplication.properties

--- spring/src/main/java/com/spring/spring/config/SecurityConfig.java
+++ spring/src/main/java/com/spring/spring/config/SecurityConfig.java
@@ -1,8 +1,14 @@
 package com.spring.spring.config;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.authentication.OidcIdTokenDecoderFactory;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.jwt.JwtDecoderFactory;
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,13 +24,13 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .oauth2Login(oauth2 -> oauth2
-                .loginPage("/login")                    //Página de login personalizada
-                .defaultSuccessUrl("/students", true)   //Redireciona após login
-                .failureUrl("/login?error")             //Redireciona se falhar
+                .loginPage("/login")
+                .defaultSuccessUrl("/students", true)
+                .failureUrl("/login?error")
             )
             .logout(logout -> logout
-                .logoutUrl("/logout")                   //Endpoint Spring para logout
-                .logoutSuccessUrl("/google-logout")     //Redireciona para nosso controlador Google
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/google-logout")
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .deleteCookies("JSESSIONID")
@@ -32,5 +38,15 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoderFactory<ClientRegistration> idTokenDecoderFactory() {
+        OidcIdTokenDecoderFactory factory = new OidcIdTokenDecoderFactory();
+        factory.setJwtValidatorFactory(clientRegistration -> {
+            JwtTimestampValidator timestampValidator = new JwtTimestampValidator(Duration.ofMinutes(60));
+            return timestampValidator;
+        });
+        return factory;
     }
 }
